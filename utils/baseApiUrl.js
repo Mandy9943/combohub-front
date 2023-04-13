d202784acc02cce4a33477096aa06710672e92d6
Armando Cesar Martin Calderón <armandoc9943business@gmail.com>
2023-04-13 04:26:03 -0400

    update api url for production

--- utils/baseApiUrl.js
+++ utils/baseApiUrl.js
@@ -1,5 +1,6 @@
-const baseApiUrl = process.env.NODE_ENV === "production" 
-? 'http://localhost:1337' 
-: 'http://localhost:1337';
+const baseApiUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://api.combohub.website"
+    : "http://localhost:1337";
 
 export default baseApiUrl;
